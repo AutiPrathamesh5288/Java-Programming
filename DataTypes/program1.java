62b2cd090dbdb2f8551b5086cee15084783b7485
Prathameshauti <prathameshauti004@gmail.com>
2023-07-11 12:24:54 +0530

    DataTypes Codes Java

---
+++ DataTypes/program1.java
@@ -0,0 +1,14 @@
+class Core2web{
+	public static void main(String[] args){
+		byte var1=18;
+		byte var2=18;
+		System.out.println(var1);
+		System.out.println(var2);
+
+		var1=var1+var2;
+		System.out.println(var1);
+System.out.println(var2);
+
+		}
+}
+
