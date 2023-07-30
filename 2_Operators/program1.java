933e99320410846eb005a1a560bc61c5311b187d
Prathameshauti <prathameshauti004@gmail.com>
2023-07-30 23:00:49 +0530

    Operators in Java

---
+++ 2_Operators/program1.java
@@ -0,0 +1,12 @@
+class Core2web{
+	public static void main(String [] rags){
+		int x=20;
+		int y=30;
+
+		System.out.println(x+y);
+		System.out.println(x-y);
+		System.out.println(x*y);
+		System.out.println(x/y);
+		System.out.println(x%y);
+	}
+}
---
+++ 2_Operators/program3.java
@@ -0,0 +1,10 @@
+class Core2web{
+	public static void main(String[] rgas){
+		int x=20;
+		int y=30;
+
+		if(x<y);{
+			System.out.println("hello");
+	}
+	}
+}
