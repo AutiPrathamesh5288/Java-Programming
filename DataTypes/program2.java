1447326ec4be546be83a90ad60f264a2c20266c0
Prathameshauti <prathameshauti004@gmail.com>
2023-07-30 22:49:42 +0530

    Deleted

--- DataTypes/program2.java
+++
@@ -1,8 +0,0 @@
-class Core2web{
-        public static void main(String[] args){
-		float f1=7;
-		float f2=8.3f;
-		System.out.println(f1);
-		System.out.println(f2);
-	}
-}
--- Operators/Ternary.java
+++
@@ -1,9 +0,0 @@
-class Core2Web{
-	public static void main(String[] args){
-
-		int x=10;
-		int y=20;
-
-		System.out.println((x>y)? x:y);
-	}
-}
