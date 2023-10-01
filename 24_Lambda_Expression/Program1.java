c5351bf9cdb713f6b0a217f9301675f72eaabeec
AutiPrathamesh5288 <prathameshauti004@gmail.com>
2023-10-01 16:23:39 +0530

    Lambda Expression in java

---
+++ 24_Lambda_Expression/Program1.java
@@ -0,0 +1,16 @@
+interface Core2web {
+
+	void lang();
+}
+class Year2022{
+	public static void main(String[] args){
+
+		Core2web c2w = ()-> {
+
+			System.out.println("Java/C++/C");
+
+		};
+		c2w.lang();
+
+	}
+}
